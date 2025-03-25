67e92c1bc242fc1cedafa537c18dcfb30ff2ea96
Pablo Nudel <pablonudel@gamil.com>
2025-03-25 16:29:32 +0100

    Remove redundant getter for sqft in SkyHighBuilding class

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -21,10 +21,6 @@ export default class SkyHighBuilding extends Building {
     return this._floors;
   }
 
-  get sqft() {
-    return this._sqft;
-  }
-
   evacuationWarningMessage() {
     return `Evacuate slowly the ${this._floors} floors`;
   }
