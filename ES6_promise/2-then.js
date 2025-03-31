a80d8737dd49d82b4dd86e94e6049c1ae383c599
Pablo Nudel <pablonudel@gamil.com>
2025-03-31 18:24:10 +0200

    Refactor promise handling in API response functions for improved clarity and error handling

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,5 +1,15 @@
 export default function handleResponseFromAPI(promise) {
-  promise
-    .then(() => console.log('Got a response from the API'))
-    .catch(() => console.log('Got a response from the API'));
+  const msg = 'Got a response from the API';
+  return promise
+    .then(() => {
+      console.log(msg);
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch(() => {
+      console.log(msg);
+      return new Error();
+    });
 }
--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,11 +3,10 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => {
-      const response = [];
-      results.forEach((result) => {
-        response.push({ status: result.status, value: result.value });
-      });
-      return response;
-    });
+    .then((results) => results.map((promise) => ({
+      status: promise.status,
+      value: promise.status === 'fulfilled'
+        ? promise.value
+        : `${promise.reason.name}: ${promise.reason.message}`,
+    })));
 }
