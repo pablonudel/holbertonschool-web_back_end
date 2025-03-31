63d77f8874670db670bb00ae34be38f565bf405d
Pablo Nudel <pablonudel@gamil.com>
2025-03-31 14:49:55 +0200

    Remove unnecessary check for empty startString in cleanSet function

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,4 @@
 export default function cleanSet(set, startString) {
-  if (startString.length === 0) {
-    return '';
-  }
   const strings = [...set]
     .filter((str) => str && str.startsWith(startString))
     .map((str) => str.slice(startString.length));
