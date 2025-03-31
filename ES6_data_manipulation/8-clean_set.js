929127363597c0899a208bff5e9b31dc98cf6da4
Pablo Nudel <pablonudel@gamil.com>
2025-03-31 14:52:31 +0200

    Add validation for startString in cleanSet function

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,7 @@
 export default function cleanSet(set, startString) {
+  if (typeof startString !== 'string' || startString.length === 0) {
+    return '';
+  }
   const strings = [...set]
     .filter((str) => str && str.startsWith(startString))
     .map((str) => str.slice(startString.length));
