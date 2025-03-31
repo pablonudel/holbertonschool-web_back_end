6733e7f443c644006e8751476372ebd5e4a1d9d9
Pablo Nudel <pablonudel@gamil.com>
2025-03-31 14:46:47 +0200

    Refactor getStudentsByLocation function to use filter for improved readability

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,9 +1,3 @@
 export default function getStudentsByLocation(students, city) {
-  const newList = [];
-  for (const student of students) {
-    if (student.location === city) {
-      newList.push(student);
-    }
-  }
-  return newList;
+  return students.filter((student) => student.location === city);
 }
