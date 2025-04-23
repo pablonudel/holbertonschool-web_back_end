ea261f43dddf9434ce6610e764c2173b9080dc44
pablonudel <pablonudel@gmail.com>
2025-04-23 17:40:45 +0200

    Enhance docstring for wait_n function to clarify return value description

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """Module for the wait_n function"""
-import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """return the list of all the delays (float values)
+    sorted in ascending order"""
     list = []
     for i in range(n):
         delay = await wait_random(max_delay)
