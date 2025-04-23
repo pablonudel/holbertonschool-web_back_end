412aa6a5f9e32aae15dcaf0cb8ca8bc6de2e6214
pablonudel <pablonudel@gmail.com>
2025-04-23 17:31:10 +0200

    Refactor wait_n function to use a dedicated sort_list function for improved clarity and maintainability

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -4,11 +4,20 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
+def sort_list(delay_list: list):
+    """Sort a list in ASC"""
+    list_len: int = len(delay_list)
+    for i in range(list_len - 1):
+        for j in range(list_len - 1 - i):
+            if delay_list[j] > delay_list[j + 1]:
+                delay_list[j], delay_list[j + 1] = delay_list[j + 1], delay_list[j]
+    return delay_list
+
 async def wait_n(n: int, max_delay: int) -> List[int]:
     """Return a sorted list of all the delays (float values) got from
     the wait_random function executed n times"""
-    list = []
+    delay_list: list = []
     for i in range(n):
         delay = await wait_random(max_delay)
-        list.append(delay)
-    return sorted(list)
+        delay_list.append(delay)
+    return sort_list(delay_list)
