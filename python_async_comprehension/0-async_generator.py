a28e288a5532e90ca2034bdcb81737f650ee3246
pablonudel <pablonudel@gmail.com>
2025-04-28 12:07:15 +0200

    Refactor async_generator function to use underscore for unused loop variable

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,6 +8,6 @@ from typing import AsyncGenerator
 async def async_generator() -> AsyncGenerator[float]:
     """coroutine that loop 10 times, each time asynchronously wait 1 second,
     then yield a random number between 0 and 10."""
-    for i in range(10):
+    for _ in range(10):
         yield random.uniform(0, 10)
         await asyncio.sleep(1)
