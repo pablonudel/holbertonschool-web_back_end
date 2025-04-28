5a7c2465f5370ba083b8b5a62ac3d11b3d418132
pablonudel <pablonudel@gmail.com>
2025-04-28 12:15:28 +0200

    Reorder yield and sleep in async_generator for correct execution flow

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -9,5 +9,5 @@ async def async_generator() -> AsyncGenerator[float]:
     """coroutine that loop 10 times, each time asynchronously wait 1 second,
     then yield a random number between 0 and 10."""
     for _ in range(10):
-        yield random.uniform(0, 10)
         await asyncio.sleep(1)
+        yield random.uniform(0, 10)
