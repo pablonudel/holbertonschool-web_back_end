a17807052de1b1fe9cd31f9652f3e481bea72740
pablonudel <pablonudel@gmail.com>
2025-04-29 10:08:40 +0200

    Add update_topics and insert_school functions for managing school documents in NoSQL

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module for update_topics function"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """changes all topics of a school document based on the name"""
+    return mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
