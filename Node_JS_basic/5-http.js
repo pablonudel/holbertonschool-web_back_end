d77767b166ea6ac0264074a220239b25d8b5595e
pablonudel <pablonudel@gmail.com>
2025-05-19 16:35:31 +0200

    Refactor countStudents function to read student data from file and format output

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,8 +1,35 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs').promises;
 
 const db = process.argv.slice(2)[0];
 const port = 1245;
+
+async function countStudents(path) {
+  try {
+    const data = await fs.readFile(path, 'utf8');
+    const rows = data.trim().split('\n').slice(1);
+    const fields = {};
+
+    rows.forEach((row) => {
+      const columns = row.split(',');
+      const [firstname, , , field] = columns;
+      if (firstname && field) {
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+      }
+      fields[field].push(firstname);
+    });
+
+    let output = `Number of students: ${rows.length}`;
+    for (const [field, students] of Object.entries(fields)) {
+      output += `\nNumber of students in ${field}: ${students.length}. List: ${students.join(', ')}`;
+    }
+    return output;
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
 
 const app = http.createServer(async (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
