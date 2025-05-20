d4e74584483b8cb141fde7bed5adeccd20d12c4a
pablonudel <pablonudel@gmail.com>
2025-05-20 13:06:58 +0200

    Refactor AppController.getHomepage method parameter names for consistency; remove dev.js file

--- Node_JS_basic/full_server/routes/index.js
+++ Node_JS_basic/full_server/routes/index.js
@@ -1,4 +1,4 @@
-const express = require('express')
+import express from 'express';
 import AppController from '../controllers/AppController';
 import StudentsController from '../controllers/StudentsController';
 
