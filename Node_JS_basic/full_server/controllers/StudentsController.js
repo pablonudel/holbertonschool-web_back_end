6093f95b8d9d7b15fce9389e4e9f134076080792
pablonudel <pablonudel@gmail.com>
2025-05-20 11:14:08 +0200

    Refactor getAllStudents method to handle empty database case; fix response variable in getAllStudentsByMajor method

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -4,17 +4,13 @@ class StudentsController {
   static async getAllStudents(request, response) {
     try {
       const data = await readDatabase(request.app.get('database'));
-      
       let output = 'This is the list of our students';
-      
-      if (Object.keys(data).length === 0) {
-        output += '\nNo students in database';
-      } else {
-        for (const field in data) {
-          const students = data[field];
-          output += `\nNumber of students in ${field}: ${students.length}. List: ${students.join(', ')}`;
-        }
+
+      for (const field in data) {
+        const students = data[field];
+        output += `\nNumber of students in ${field}: ${students.length}. List: ${students.join(', ')}`;
       }
+
       response.status(200).send(output);
     } catch (error) {
       response.status(500).send(error.message);
@@ -32,7 +28,7 @@ class StudentsController {
       const data = await readDatabase(request.app.get('database'));
       const students = data[major]
       if (!students) {
-        return res.status(500).send('Cannot load the database');
+        return response.status(500).send('Cannot load the database');
       }
 
       return response.status(200).send(`List: ${students.join(', ')}`);
