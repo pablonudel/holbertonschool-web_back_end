5c092f53ee60f006d37e255a687f8c088cea267e
pablonudel <pablonudel@gmail.com>
2025-05-20 12:40:45 +0200

    Refactor getAllStudents and getAllStudentsByMajor methods for improved consistency; update parameter naming and response handling

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,39 +1,41 @@
 import readDatabase from '../utils';
 
 class StudentsController {
-  static async getAllStudents(request, response) {
+  static async getAllStudents(req, res) {
     try {
-      const data = await readDatabase(request.app.get('database'));
-      let output = 'This is the list of our students';    
+      const data = await readDatabase(req.app.get('database'));
+      let output = 'This is the list of our students';
 
-      for (const field in data) {
+      const sortedFields = Object.keys(data).sort((a, b) => a.localeCompare(b, undefined, { sensitivity: 'base' }));
+
+      for (const field of sortedFields) {
         const students = data[field];
         output += `\nNumber of students in ${field}: ${students.length}. List: ${students.join(', ')}`;
       }
 
-      return response.status(200).send(output);
+      return res.status(200).send(output);
     } catch (error) {
-      return response.status(500).send(error.message);
+      return res.status(500).send(error.message);
     }
   }
 
-  static async getAllStudentsByMajor(request, response) {
-    const { major } = request.params;
+  static async getAllStudentsByMajor(req, res) {
+    const { major } = req.params;
 
-    if (major !== 'CS' || major !== 'SWE') {
-      return response.status(500).send('Major parameter must be CS or SWE');
+    if (!['CS', 'SWE'].includes(major)) {
+      return res.status(500).send('Major parameter must be CS or SWE');
     }
 
     try {
-      const data = await readDatabase(request.app.get('database'));
-      const students = data[major]
+      const data = await readDatabase(req.app.get('database'));
+      const students = data[major];
       if (!students) {
-        return response.status(500).send('Cannot load the database');
+        return res.status(500).send('Cannot load the database');
       }
 
-      return response.status(200).send(`List: ${students.join(', ')}`);
+      return res.status(200).send(`List: ${students.join(', ')}`);
     } catch (error) {
-      return response.status(500).send(error.message);
+      return res.status(500).send(error.message);
     }
   }
 }
