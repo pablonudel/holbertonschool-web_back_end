00840f5ebd408e2d5d4f40fb9198cf7907186fa8
pablonudel <pablonudel@gmail.com>
2025-05-20 11:48:32 +0200

    Refactor response handling in getAllStudents method for consistency

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -11,9 +11,9 @@ class StudentsController {
         output += `\nNumber of students in ${field}: ${students.length}. List: ${students.join(', ')}`;
       }
 
-      response.status(200).send(output);
+      return response.status(200).send(output);
     } catch (error) {
-      response.status(500).send(error.message);
+      return response.status(500).send(error.message);
     }
   };
 
